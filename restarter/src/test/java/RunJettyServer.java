682725f2832c4077b95744260125db93e907d896
quanet <quanet@163.com>
2014-02-21 05:30:09 +0800

    提交初始化代码

--- restarter/src/test/java/RunJettyServer.java
+++ restarter/src/test/java/RunJettyServer.java
@@ -14,16 +14,22 @@ public class RunJettyServer {
      */
     private static final int PORT = 8080;
     private static final String CONTEXT = "/basic";
-    private static final String[] TLD_JAR_NAMES = new String[] {};
+    private static final String[] TLD_JAR_NAMES = new String[] {"sitemesh", "spring-webmvc"};
 
 
 
     public static void main(String[] args) throws Exception {
 
         Server server = JettyFactory.createServer(PORT,CONTEXT);
+        JettyFactory.setTldNames(server, TLD_JAR_NAMES);
 
         try {
             //当控制台中输入回车的时候,应用重启
+
+            server.start();
+            System.out.println("Jetty Server running at http://localhost:" + PORT + CONTEXT);
+            System.out.println("Hit Enter to reload the application quickly");
+
             while(true) {
                 char c = (char) System.in.read();
                 if(c=='\n') {
