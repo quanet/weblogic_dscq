fbe6629a533e95ebd195472c21f7f82cc2a0f562
quanet <quanet@163.com>
2014-02-21 04:22:43 +0800

    提交初始化代码

--- restarter/src/test/java/RunJettyServer.java
+++ restarter/src/test/java/RunJettyServer.java
@@ -1,3 +1,6 @@
+import cn.com.jetty.JettyFactory;
+import org.eclipse.jetty.server.Server;
+
 /**
  * @author : lihaoquan
  *
@@ -6,9 +9,30 @@
  */
 public class RunJettyServer {
 
-     public static void main(String[] args) throws Exception {
+    /**
+     * 相关启动参数
+     */
+    private static final int PORT = 8080;
+    private static final String CONTEXT = "/basic";
+    private static final String[] TLD_JAR_NAMES = new String[] {};
 
 
 
-     }
+    public static void main(String[] args) throws Exception {
+
+        Server server = JettyFactory.createServer(PORT,CONTEXT);
+
+        try {
+            //当控制台中输入回车的时候,应用重启
+            while(true) {
+                char c = (char) System.in.read();
+                if(c=='\n') {
+                    JettyFactory.reloadContext(server);
+                }
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
 }
--- restarter/src/test/java/cn/com/jetty/JettyFactory.java
+++ restarter/src/test/java/cn/com/jetty/JettyFactory.java
@@ -1,7 +1,37 @@
 package cn.com.jetty;
 
+import org.eclipse.jetty.server.Server;
+
 /**
  * @author : lihaoquan
  */
 public class JettyFactory {
+
+
+    /**
+     * 创建Jetty的Server
+     * @param port
+     * @param contextPath
+     * @return
+     */
+    public static Server createServer(int port,String contextPath) {
+        return null;
+    }
+
+
+    /**
+     * 设置相关Jar包的名称
+     */
+    public static void setTldNames() {
+
+    }
+
+
+    /**
+     * 用做重启applicationContext
+     */
+    public static void reloadContext(Server server) {
+
+        System.out.println("正在进行重启操作... ...");
+    }
 }
